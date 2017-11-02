0ae0fbefe902772149e25938433b2477c818874d
Danceiny <danceiny@gmail.com>
2017-11-02 20:38:26 +0800

    [DEV] hello world

---
+++ Application/User/user_views.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from flask_restful import Resource, reqparse
+from flask import g, jsonify, Response, stream_with_context, render_template, send_file, Response
+from Library import error_util as ED
+from itertools import chain
+
+class User_API(Resource):
+    def get(self):
+        return {'code': 200, 'data': 'hello world'}
--- Application/api.py
+++ Application/api.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 from app import flask_app
 api = Api(flask_app)
-
+from Application.User.user_views import User_API
 
 api.add_resource(User_API, '/api/v1/user', endpoint='user')
