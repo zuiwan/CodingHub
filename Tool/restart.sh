9828d50c8935accce5f1343d4263931bf704f8ea
Danceiny <danceiny@gmail.com>
2017-11-04 15:28:22 +0800

    [DEBUG] deploy

--- Tool/restart.sh
+++ Tool/restart.sh
@@ -5,7 +5,7 @@ rm logs/*_supervisor.log
 #docker service rm $(docker service ls | awk '{print $1}')
 
 #supervisord
-supervisorctl restart uwsgi
+supervisorctl restart uwsgi:CodingLife
 #supervisorctl restart celery_cpu
 #supervisorctl restart celery_gpu
 #supervisorctl restart celery_fork
