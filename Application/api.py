8e8e74869531e73378d518502f55de521bca3e6d
Danceiny <danceiny@gmail.com>
2017-11-04 20:24:54 +0800

    [DEV] 负载均衡

--- Application/api.py
+++ Application/api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask import jsonify
 from flask_restful import Api
 from Application.app import flask_app
 
@@ -7,3 +8,7 @@ api = Api(flask_app)
 from Application.User.user_views import User_API
 
 api.add_resource(User_API, '/api/v1/user', endpoint='user')
+
+@flask_app.route('/', methods=['GET'])
+def index():
+    return jsonify(dict(message="Hello World"))
