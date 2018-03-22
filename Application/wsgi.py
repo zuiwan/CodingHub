7df5fd014e3224431906ad16f39b70716c1469a7
Danceiny <danceiny@gmail.com>
2018-03-23 00:25:26 +0800

    [DEV] templates folder

--- Application/wsgi.py
+++ Application/wsgi.py
@@ -1,10 +1,5 @@
 #!/usr/in/env python
 # -*- coding: utf-8 -*-
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # 把项目目录加载到$PYTHONPATH中
-
 from Application.api import flask_app
 
 if __name__ == '__main__':
--- tool/database_manager.py
+++ tool/database_manager.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import sys
-
 sys.path.append(".")
 sys.path.append("..")
-from Application.api import flask_app
 from Library.extensions import orm
+from Application.api import flask_app
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
